84e6786a32df83c0b401e5b517c5c07946ba0ea0
ichiren1 <ichiren1@gmail.com>
2016-04-25 18:09:24 +0900

    ユーザーエージェントに関する記述の削除

--- js/index.js
+++ js/index.js
@@ -1,22 +1,5 @@
 var nameInputCnt = 2;
 var numInputCnt = 3;
-var _ua = (function(u){
-  return {
-    Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1) 
-      || u.indexOf("ipad") != -1
-      || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
-      || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
-      || u.indexOf("kindle") != -1
-      || u.indexOf("silk") != -1
-      || u.indexOf("playbook") != -1,
-    Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
-      || u.indexOf("iphone") != -1
-      || u.indexOf("ipod") != -1
-      || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
-      || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
-      || u.indexOf("blackberry") != -1
-  }
-})(window.navigator.userAgent.toLowerCase());
 
 function setDeleteButtonHeight(){
   $('.delete_button').height($('.input_fields').height());
